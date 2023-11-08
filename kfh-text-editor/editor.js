7609146fa06591234a53b71b73c296928d0710e9
programmerKFH <kfh.freelancer@gmail.com>
2023-11-07 23:57:31 -0400

    Adding more code to this file.

--- kfh-text-editor/editor.js
+++ kfh-text-editor/editor.js
@@ -1,38 +1,41 @@
-// Variables globales
-let texto = document.getElementById("texto");
-let botones = document.getElementById("botones");
+document.addEventListener("DOMContentLoaded", function() {
+  
+  // Variables globales
+  let texto = document.getElementById("texto");
+  let botones = document.getElementById("botones");
 
-// Función para crear un nuevo documento
-function nuevo() {
-  texto.value = "";
-}
-
-// Función para abrir un documento existente
-function abrir() {
-  let file = window.prompt("Elige un archivo:");
-  if (file) {
-    // Cargamos el contenido del archivo en el editor
-    texto.value = new FileReader().readAsText(file).toString();
+  // Función para crear un nuevo documento
+  function nuevo() {
+    texto.value = "";
   }
-}
 
-// Función para guardar el documento
-function guardar() {
-  let file = window.prompt("Escribe el nombre del archivo:");
-  if (file) {
-    // Guardamos el contenido del editor en el archivo
-    new FileWriter(file).write(texto.value);
+  // Función para abrir un documento existente
+  function abrir() {
+    let file = window.prompt("Elige un archivo:");
+    if (file) {
+      // Cargamos el contenido del archivo en el editor
+      texto.value = new FileReader().readAsText(file).toString();
+    }
   }
-}
 
-// Eventos
-botones.addEventListener("click", function(event) {
-  let elemento = event.target;
-  if (elemento.id === "nuevo") {
-    nuevo();
-  } else if (elemento.id === "abrir") {
-    abrir();
-  } else if (elemento.id === "guardar") {
-    guardar();
+  // Función para guardar el documento
+  function guardar() {
+    let file = window.prompt("Escribe el nombre del archivo:");
+    if (file) {
+      // Guardamos el contenido del editor en el archivo
+      new FileWriter(file).write(texto.value);
+    }
   }
+
+  // Eventos
+  botones.addEventListener("click", function(event) {
+    let elemento = event.target;
+    if (elemento.id === "nuevo") {
+      nuevo();
+    } else if (elemento.id === "abrir") {
+      abrir();
+    } else if (elemento.id === "guardar") {
+      guardar();
+    }
+  });
 });
