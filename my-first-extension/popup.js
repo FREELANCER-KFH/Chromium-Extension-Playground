d1b6af590b97eb2e07edb4d89aeb323a449cd3ac
programmerKFH <kfh.freelancer@gmail.com>
2023-11-10 23:59:09 -0400

    Adding other functions to the project.

--- my-first-extension/popup.js
+++ my-first-extension/popup.js
@@ -6,6 +6,14 @@ document.addEventListener('DOMContentLoaded', function () {
   let btnCambiarSize = document.getElementById('cambiarSize')
   let inputSize = document.getElementById('size')
   btnCambiarSize.addEventListener('click', cambiarSize(inputSize.value));
+
+  let btnCambiarFont = document.getElementById('cambiarFont')
+  let inputFont = document.getElementById('font')
+  btnCambiarFont.addEventListener('click', cambiarFont(inputFont.value));
+
+  let btnCambiarBackground = document.getElementById('cambiarBackground')
+  let inputBackground = document.getElementById('background')
+  btnCambiarBackground.addEventListener('click', cambiarBackground(inputBackground.value));
 });
 
 function cambiarColor(input) { 
@@ -16,4 +24,14 @@ function cambiarColor(input) {
 function cambiarSize(inputSize) { 
   let size = document.body.style
   size.fontSize=inputSize.value
+}
+
+function cambiarFont(inputFont) { 
+  let font = document.body.style
+  font.fontFamily=inputFont.value
+}
+
+function cambiarBackground(inputBackground) {
+  let background = document.body.style
+  background.backgroundImage=inputBackground.value
 }
