b226258e3aced1dea71cef6e87d5fcf71a9d61f5
programmerKFH <kfh.freelancer@gmail.com>
2023-11-10 23:53:22 -0400

    Adding the Size changing function to the project.

--- my-first-extension/popup.js
+++ my-first-extension/popup.js
@@ -2,9 +2,18 @@ document.addEventListener('DOMContentLoaded', function () {
   let btnCambiarColor = document.getElementById('cambiarColor')
   let input = document.getElementById('color')
   btnCambiarColor.addEventListener('click', cambiarColor(input.value)); 
+
+  let btnCambiarSize = document.getElementById('cambiarSize')
+  let inputSize = document.getElementById('size')
+  btnCambiarSize.addEventListener('click', cambiarSize(inputSize.value));
 });
 
 function cambiarColor(input) { 
   let color = document.body.style
   color.backgroundColor=input.value
+}
+
+function cambiarSize(inputSize) { 
+  let size = document.body.style
+  size.fontSize=inputSize.value
 }
