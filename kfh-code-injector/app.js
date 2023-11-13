f9bbc482f9c32ae89cc22bee00ac44bb4fd1d264
programmerKFH <kfh.freelancer@gmail.com>
2023-11-13 03:18:07 -0400

    Playing with some JS alert injections

--- kfh-code-injector/app.js
+++ kfh-code-injector/app.js
@@ -1,4 +1,19 @@
 // Get the URL of the current tab
 const site = window.location.hostname;
 
-alert("KFH Code Injector is running on " + site + ".")
+//alert("KFH Code Injector is running on " + site + ".")
+
+// JS code for youtube.com
+if (site.includes("youtube.com")) {
+    alert("Whats up YouTube!")
+}
+
+// JS code for google.com
+if (site.includes("google.com")) {
+    alert("Whats the heaven up Google!")
+}
+
+// JS code for microsoft.com
+if (site.includes("microsoft.com")) {
+    alert("Whats the hell up Microsoft!")
+}
