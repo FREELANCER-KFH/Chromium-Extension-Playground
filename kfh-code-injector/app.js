e28548393d578e451b9559a2a9f7434282f706f5
programmerKFH <kfh.freelancer@gmail.com>
2023-11-13 05:52:56 -0400

    Deleting extra space lines and finishing the base project.

--- kfh-code-injector/app.js
+++ kfh-code-injector/app.js
@@ -5,39 +5,33 @@ const site = window.location.hostname;
 
 
 // Add custom CSS - function
-const add_Custon_Style = css => document.head.appendChild(document.createElement("style")).innerHTML = css;
+const add_Custom_Style = css => document.head.appendChild(document.createElement("style")).innerHTML = css;
 
 // Add custom element - function
-function Create_Custom_Element(tag, attr_tag, attr_name, value){
+function create_Custom_Element(tag, attr_tag, attr_name, value){
     const custom_element = document.createElement(tag);
     custom_element.setAttribute(attr_tag, attr_name);
     custom_element.innerHTML = value;
     document.body.append(custom_element);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
 // JS code for youtube.com
 if (site.includes("youtube.com")) {
-    alert("Whats up YouTube!")
+    alert("Whats up ${site}!")
+    add_Custom_Style("body {background-color: red !important;}");
+    create_Custom_Element("h1", "class", "my_class", "Hello ${site}!");
 }
 
 // JS code for google.com
 if (site.includes("google.com")) {
     alert("Whats the heaven up Google!")
+    add_Custom_Style("body {background-color: blue !important;}");
+    create_Custom_Element("h1", "class", "my_class", "Hello ${site}!");
 }
 
 // JS code for microsoft.com
 if (site.includes("microsoft.com")) {
     alert("Whats the hell up Microsoft!")
+    add_Custom_Style("body {background-color: green !important;}");
+    create_Custom_Element("h1", "class", "my_class", "Hello ${site}!");
 }
