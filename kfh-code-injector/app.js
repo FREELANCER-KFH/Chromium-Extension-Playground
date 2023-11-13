1bae135429f9376556c77128ed044960fc1dd5db
programmerKFH <kfh.freelancer@gmail.com>
2023-11-13 03:33:59 -0400

    Developing a custom style and a custom element.

--- kfh-code-injector/app.js
+++ kfh-code-injector/app.js
@@ -3,6 +3,30 @@ const site = window.location.hostname;
 
 //alert("KFH Code Injector is running on " + site + ".")
 
+
+// Add custom CSS - function
+const add_Custon_Style = css => document.head.appendChild(document.createElement("style")).innerHTML = css;
+
+// Add custom element - function
+function Create_Custom_Element(tag, attr_tag, attr_name, value){
+    const custom_element = document.createElement(tag);
+    custom_element.setAttribute(attr_tag, attr_name);
+    custom_element.innerHTML = value;
+    document.body.append(custom_element);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 // JS code for youtube.com
 if (site.includes("youtube.com")) {
     alert("Whats up YouTube!")
